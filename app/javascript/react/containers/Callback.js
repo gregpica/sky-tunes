bc1b3831478b323405f0ea54f8b367550713e65a
gregpica <gpica@bu.edu>
2018-08-07 15:03:38 -0400

    Create error message for cancel Spotify connect

--- app/javascript/react/containers/Callback.js
+++ app/javascript/react/containers/Callback.js
@@ -3,25 +3,42 @@ import { BrowserRouter } from 'react-router-dom';
 import storage from '../util/storage';
 import userClient from '../clients/user';
 import { JWT } from '../constants';
+import Login from './Login'
 
 class Callback extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.state = {
+      error: null
+    }
   }
 
   componentDidMount(){
     const queryString = require('query-string')
-    const code = queryString.parse(this.props.location.search).code
-    userClient.post(code)
-      .then(response => response.json())
-      .then(body => storage.set(JWT, body))
-      .then(() => this.props.history.push('/track-player'))
-      .catch(error => console.error(`Error in fetch: ${error.message}`));
+    const parsedQuerySearch = queryString.parse(this.props.location.search)
+    if(parsedQuerySearch.error) {
+      this.setState({ error: "You must connect to Spotify in order to user SkyTunes!" });
+    } else {
+      userClient.post(parsedQuerySearch.code)
+        .then(response => response.json())
+        .then(body => storage.set(JWT, body))
+        .then(() => this.props.history.push('/track-player'))
+        .catch(error => console.error(`Error in fetch: ${error.message}`));
+    }
   }
 
   render() {
+    let callBackElement = <div></div>
+    if (this.state.error) {
+      callBackElement = <Login
+                          error={this.state.error}
+                        />
+    }
+
     return(
-      <div></div>
+      <div>
+        {callBackElement}
+      </div>
     )
   }
 }
--- app/javascript/react/containers/Login.js
+++ app/javascript/react/containers/Login.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 const Login = props => {
+  let errorDiv;
+
+  if (props.error) {
+    errorDiv  = <p>{props.error}</p>
+  }
+
   return (
-    <a href="/api/v1/login"><button>Log in</button></a>
+    <div>
+      {errorDiv}
+      <a href="/api/v1/login"><button>Log in</button></a>
+    </div>
   )
 }
 
