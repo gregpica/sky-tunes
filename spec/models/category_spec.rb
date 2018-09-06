afcec147b3be9fa6bb93baee22b80b1a105b6721
gregpica <gpica@bu.edu>
2018-09-06 13:19:06 -0400

    Test get_categories Category class method

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  before(:each) do
+    load "#{Rails.root}/db/seeds.rb"
+  end
 
   describe "validations" do
     it { should have_valid(:name).when("Sunny") }
@@ -10,6 +13,16 @@ RSpec.describe Category, type: :model do
   describe "#get_timezone" do
     it "returns the appropriate timezone" do
       expect(Category.get_timezone("40.2349,-74.6546")).to eq "America/New_York"
+    end
+  end
+
+  describe "#get_categories" do
+    it "returns an array of category ids" do
+      categories = Category.get_categories("clear_day", "America/New_York")
+      expect(categories.length).to be > 1
+      categories.each do |category|
+        expect(Category.ids.include?(category)).to eq true
+      end
     end
   end
 
